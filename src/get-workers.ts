2e22ed04eb137de0830eb552905634de69b49824
Micah <micahscopes@gmail.com>
2024-10-22 19:46:29 -0500

    don't use workers on iOS to avoid memory leak

--- src/get-workers.ts
+++ src/get-workers.ts
@@ -5,21 +5,25 @@ import * as notWorker from "./worker";
 import { detect } from "detect-browser";
 import moize from "moize";
 
-const useWorkers = detect(navigator.userAgent)?.name !== "safari";
+const agent = detect(navigator.userAgent);
+const useWorkers = agent?.name !== "safari" && agent?.os !== "iOS";
+
 if (useWorkers) {
   console.log("Using workers");
 } else {
-  console.log("Not using workers");
+  console.warn(
+    "Not using workers due to an awful memory leak that makes WebKit crash if you use workers",
+  );
 }
 
 export const graphDb = moize.infinite((ctx) =>
-  useWorkers ? wrap(new Worker()) : notWorker.db
+  useWorkers ? wrap(new Worker()) : notWorker.db,
 );
 
 export const graphLayout = moize.infinite((ctx) =>
-  useWorkers ? wrap(new Worker()) : notWorker.layout
+  useWorkers ? wrap(new Worker()) : notWorker.layout,
 );
 
 export const graphCameraAnimation = moize.infinite((ctx) =>
-  false ? wrap(new Worker()) : notWorker.cameraAnimation
+  false ? wrap(new Worker()) : notWorker.cameraAnimation,
 );
