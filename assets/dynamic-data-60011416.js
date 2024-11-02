cf22ec8f755925192d50aaa4faa5d794d6ef6e61
Micah <micahscopes@gmail.com>
2024-11-02 10:04:31 -0500

    dynamic shape loading

---
+++ assets/dynamic-data-60011416.js
@@ -0,0 +1,2 @@
+import{r}from"./main-f00cce40.js";import"./worker-9ad80e94.js";window.addEventListener("DOMContentLoaded",()=>{for(let e of document.querySelectorAll("webscape-wanderer"))e.graphData=r(5,10)});
+//# sourceMappingURL=dynamic-data-60011416.js.map
