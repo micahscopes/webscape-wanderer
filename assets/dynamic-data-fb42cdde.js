937a4ede1937ecf1bf9fc98aa90d2ea949b6506a
Micah <micahscopes@gmail.com>
2024-11-05 16:02:24 +0700

    reduce extraneous cpu work

---
+++ assets/dynamic-data-fb42cdde.js
@@ -0,0 +1,2 @@
+import{r}from"./main-986662a8.js";import"./worker-cb3871c2.js";window.addEventListener("DOMContentLoaded",()=>{for(let e of document.querySelectorAll("webscape-wanderer"))e.graphData=r(5,10)});
+//# sourceMappingURL=dynamic-data-fb42cdde.js.map
---
+++ assets/worker-dd611fe7.js
@@ -0,0 +1,2 @@
+import{c as m,a as r,l as s}from"./worker-cb3871c2.js";export{m as cameraAnimation,r as db,s as layout};
+//# sourceMappingURL=worker-dd611fe7.js.map
